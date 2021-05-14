da728784781bae949592ec659b7ddf6e6b805f40
Nelson Medina <nelsonmedina6@gmail.com>
2021-05-13 21:20:56 -0500

    Adding migrations

---
+++ database/migrations/2021_05_14_020725_create_matches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->int('game_id');
+            $table->int('user_id'); //the winner of the match
+            $table->float('score')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
---
+++ database/migrations/2021_05_14_022001_create_match_players_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchPlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match_players', function (Blueprint $table) {
+            $table->id();
+            $table->int('player_1');
+            $table->int('player_2');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match_players');
+    }
+}
