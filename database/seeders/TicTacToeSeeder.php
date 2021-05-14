ed7a8db86dfce879c6778e1b9af7c0a8c025c3ed
Nelson Medina <nelsonmedina6@gmail.com>
2021-05-13 22:02:12 -0500

    Adding game seeder

---
+++ database/seeders/TicTacToeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Game;
+use Illuminate\Database\Seeder;
+
+class TicTacToeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $game = new Game([
+            'name' => 'TicTacToe',
+            'description' => 'A funny TicTacToe Game',
+            'rules' => 'Match three Xs or Os in a row in any direction and you win!'
+        ]);
+    }
+}
